13560d3a81a0f8d084a67a241fccbeb134fa31e3
JM <271730880+melancholymoses-dev@users.noreply.github.com>
2026-03-12 23:20:02 -0700

    update headers and wrapper for GLBackend

--- neo/renderer/RendererBackend.h
+++ neo/renderer/RendererBackend.h
@@ -10,16 +10,16 @@ struct IBackend
     virtual void PostSwapBuffers() = 0;
 
     // Resource management
-    virtual void Image_Upload(idImage *, const byte *, int w, int h) = 0;
-    virtual void Image_Purge(idImage *) = 0;
-    virtual void VertexCache_Alloc(vertCache_t *, const void *data, int size, bool indexBuffer) = 0;
-    virtual void VertexCache_Free(vertCache_t *) = 0;
+    virtual void Image_Upload(idImage *img, const byte *, int w, int h) = 0;
+    virtual void Image_Purge(idImage *img) = 0;
+    virtual void VertexCache_Alloc(vertCache_t **vc, const void *data, int size, bool indexBuffer) = 0;
+    virtual void VertexCache_Free(vertCache_t *vc) = 0;
 
     // Frame dispatch
-    virtual void DrawView(const viewDef_t *) = 0;
-    virtual void CopyRender(const copyRenderCommand_t &) = 0;
+    virtual void DrawView(const viewDef_t *view) = 0;
+    virtual void CopyRender(const copyRenderCommand_t &cmd) = 0;
 };
 
-class GLBackend : public IBackend;
-class VKBackend : public IBackend;
+class GLBackend;
+class VKBackend;
 extern IBackend *activeBackend; // set at init time
