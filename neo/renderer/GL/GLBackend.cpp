72f7edd83d252b1223d6f36e9f1d534481e9a785
JM <271730880+melancholymoses-dev@users.noreply.github.com>
2026-03-12 21:43:26 -0700

    update stubs

--- neo/renderer/GL/GLBackend.cpp
+++ neo/renderer/GL/GLBackend.cpp
@@ -1,17 +1,62 @@
 #include "renderer/RendererBackend.h"
-#include "renderer/tr_local.h"         // whatever GL headers you need
+#include "renderer/tr_local.h" // whatever GL headers you need
 
 // Forward declarations of the existing GL free functions
 // (the ones that already exist in draw_glsl.cpp, tr_backend.cpp, etc.)
-extern void RB_DrawView( const void* );
-extern void GL_UploadTexture( idImage*, const byte*, int, int );
+extern void RB_DrawView(const void *);
+extern void GL_UploadTexture(idImage *, const byte *, int, int);
 // etc.
 
-void GLBackend::DrawView( const viewDef_t* view ) {
-    RB_DrawView( view );   // just delegate to the existing function
+void GLBackend::DrawView(const viewDef_t *view)
+{
+    RB_DrawView(view); // just delegate to the existing function
 }
 
-void GLBackend::Image_Upload( idImage* img, const byte* data, int w, int h ) {
-    GL_UploadTexture( img, data, w, h );
+void GLBackend::Image_Upload(idImage *img, const byte *data, int w, int h)
+{
+    GL_UploadTexture(img, data, w, h);
 }
 // ...
+
+// might be the function we're messing with?
+void GLBackend::Init()
+{
+    common->Printf('Starting GLBackend');
+    return 0
+}
+
+void GLBackend::Shutdown()
+{
+    common->Printf('Shutting down GLBackend');
+    return 0
+}
+void GLBackend::PostSwapBuffers() = 0;
+
+// Resource management
+void GLBackend::UploadImage(idImage *, const byte *, int w, int h)
+{
+    Image_Upload(w, h);
+}
+
+void GLBackend::Image_Purge(idImage *)
+{
+    Image_Upload()
+}
+void GLBackend::VertexCache_Alloc(vertCache_t *, const void *data, int size, bool indexBuffer)
+{
+    VertexCache_Alloc(data, size, indexBuffer);
+}
+void GLBackend::VertexCache_Free(vertCache_t *)
+{
+    VertexCache_Free();
+}
+
+// Frame dispatch
+void GLBackend::DrawView(const viewDef_t *)
+{
+    DrawView();
+}
+void GLBackend::CopyRender(const copyRenderCommand_t &)
+{
+    CopyRender();
+}
