94fdd082137be4bc28b7013d2a3126dd4fabd641
JM <271730880+melancholymoses-dev@users.noreply.github.com>
2026-03-12 23:20:02 -0700

    update headers and wrapper for GLBackend

--- neo/renderer/GL/GLBackend.cpp
+++ neo/renderer/GL/GLBackend.cpp
@@ -1,62 +1,58 @@
 #include "renderer/RendererBackend.h"
 #include "renderer/tr_local.h" // whatever GL headers you need
+#include "renderer/VertexCache.h"
+#include "renderer/Image.h"
+#include "renderer/draw_common.h"
+#include "renderer/GL/GLBackend.h"
 
 // Forward declarations of the existing GL free functions
 // (the ones that already exist in draw_glsl.cpp, tr_backend.cpp, etc.)
 extern void RB_DrawView(const void *);
 extern void GL_UploadTexture(idImage *, const byte *, int, int);
-// etc.
 
-void GLBackend::DrawView(const viewDef_t *view)
+// might be the function we're messing with?
+void GLBackend::Init()
 {
-    RB_DrawView(view); // just delegate to the existing function
+    common->Printf("Starting GLBackend");
+    GLimp_Init();
+}
+
+void GLBackend::Shutdown()
+{
+    common->Printf("Shutting down GLBackend");
+    GLimp_Shutdown();
+}
+void GLBackend::PostSwapBuffers()
+{
+    GLimp_SwapBuffers();
 }
 
 void GLBackend::Image_Upload(idImage *img, const byte *data, int w, int h)
 {
     GL_UploadTexture(img, data, w, h);
 }
-// ...
 
-// might be the function we're messing with?
-void GLBackend::Init()
+void GLBackend::Image_Purge(idImage *img)
 {
-    common->Printf('Starting GLBackend');
-    return 0
+    img->PurgeImage();
 }
-
-void GLBackend::Shutdown()
+void GLBackend::VertexCache_Alloc(vertCache_t **vc, const void *data, int size, bool indexBuffer)
 {
-    common->Printf('Shutting down GLBackend');
-    return 0
+    vertexCache.Alloc(data, size, vc, indexBuffer);
 }
-void GLBackend::PostSwapBuffers() = 0;
-
-// Resource management
-void GLBackend::UploadImage(idImage *, const byte *, int w, int h)
+void GLBackend::VertexCache_Free(vertCache_t *vc)
 {
-    Image_Upload(w, h);
-}
-
-void GLBackend::Image_Purge(idImage *)
-{
-    Image_Upload()
-}
-void GLBackend::VertexCache_Alloc(vertCache_t *, const void *data, int size, bool indexBuffer)
-{
-    VertexCache_Alloc(data, size, indexBuffer);
-}
-void GLBackend::VertexCache_Free(vertCache_t *)
-{
-    VertexCache_Free();
+    vertexCache.Free(vc);
 }
 
 // Frame dispatch
-void GLBackend::DrawView(const viewDef_t *)
+
+void GLBackend::DrawView(const viewDef_t *view)
 {
-    DrawView();
+    RB_DrawView(view); // just delegate to the existing function
 }
-void GLBackend::CopyRender(const copyRenderCommand_t &)
+
+void GLBackend::CopyRender(const copyRenderCommand_t &cmd)
 {
-    CopyRender();
+    RB_CopyRender(&cmd);
 }
