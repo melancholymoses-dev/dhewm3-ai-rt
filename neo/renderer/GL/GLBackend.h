be16db88f3089546aa35e435c681af431d8e3969
JM <271730880+melancholymoses-dev@users.noreply.github.com>
2026-03-21 02:12:23 -0700

    more steps to complete disentangling vulkan and GL with ibackend.

--- neo/renderer/GL/GLBackend.h
+++ neo/renderer/GL/GLBackend.h
@@ -11,6 +11,10 @@ class GLBackend : public IBackend
     void Image_Purge(idImage *) override;
     void VertexCache_Alloc(vertCache_t **, void *, int size, bool indexBuffer) override;
     void VertexCache_Free(vertCache_t *) override;
+    void BeginCommandBatch() override;
+    void EndCommandBatch() override;
+    void SetBuffer(const void *data) override;
+    void SwapBuffers(const void *data) override;
     void DrawView(const drawSurfsCommand_t *) override;
     void CopyRender(const copyRenderCommand_t &) override;
 };
